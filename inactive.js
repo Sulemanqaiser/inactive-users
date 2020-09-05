c5d405c9d38bb6a32a67d1990d8a4a4674129d30
Sulemanqaiser <sulemanqaiser2@gmail.com>
2020-09-05 21:06:38 +0500

    Update inactive.js

--- inactive.js
+++ inactive.js
@@ -17,8 +17,22 @@ const inactiveThirdStage = () => {
     console.log("User has been inactive for the past 5-6 minutes");
 }
 
-
+var x = 1 
 // this is the main method that acts like a worker and runs every 1 minute
 setInterval(function(){
     // please write your code here
+    if (x == 1 || x== 2 ){ 
+    inactiveFirstStage()
+    }
+    
+    if (x == 3 || x == 4 ){
+    inactiveSecondStage()
+    }
+    
+    if (x== 5 || x == 6 ){
+    inactiveThirdStage = () 
+    }
+    
+    x++ ;
+    x = (x%7)+1 
 }, 1000);
